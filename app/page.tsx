72a4083f6d303b3ad91936ef2b3b06a53dce1fbe
Adrian Solarczyk <95854586+adrian-solarczyk-hippo@users.noreply.github.com>
2023-11-28 17:01:11 +0100

    Update footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Conferences from '@/components/conferences';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import { CONFS_TECH_URL } from './getConferences';
 
 export const metadata: Metadata = {
   title: 'JavaScript Events and Conferences',
@@ -22,9 +23,11 @@ export default function Home() {
         <p className="text-gray-500 text-center text-xs md:text-md px-4">
           Data aggregated from{' '}
           <Link
-            href=""
+            href={CONFS_TECH_URL}
             className="dark:text-blue-300 dark:hover:text-blue-500 text-blue-600 hover:text-blue-800 transition"
-          ></Link>
+          >
+            confs.tech/javascript
+          </Link>
           .
         </p>
       </footer>
