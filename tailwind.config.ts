2deb5dcb31c10bad4e1266cabe1993642159b441
Adrian Solarczyk <95854586+adrian-solarczyk-hippo@users.noreply.github.com>
2023-11-28 10:10:03 +0100

    Fancy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            dropShadow: {
+                DEFAULT: `0 0 8px rgba(var(--drop-shadow-rgb), 0.65)`,
+                xs: `0 0 4px rgba(var(--drop-shadow-rgb), 0.55)`,
+                sm: `0 0 6px rgba(var(--drop-shadow-rgb), 0.6)`,
+                md: `0 0 10px rgba(var(--drop-shadow-rgb), 0.65)`,
+                lg: `0 0 12px rgba(var(--drop-shadow-rgb), 0.7)`,
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
